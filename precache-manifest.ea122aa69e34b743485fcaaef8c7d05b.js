e1abb737b0db29f7b2f03b3c98cad2d61365b3f8
CTLAM <CTLAM>
2021-01-11 10:31:38 +0700

    feat: layout react js

---
+++ precache-manifest.ea122aa69e34b743485fcaaef8c7d05b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3922c48339c4be691bbcb8a279a811fb",
+    "url": "/nestjs_root/index.html"
+  },
+  {
+    "revision": "987cb42770eefc193e96",
+    "url": "/nestjs_root/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "4e9c0e337136ed6cf966",
+    "url": "/nestjs_root/static/css/main.fce36487.chunk.css"
+  },
+  {
+    "revision": "987cb42770eefc193e96",
+    "url": "/nestjs_root/static/js/2.e034303b.chunk.js"
+  },
+  {
+    "revision": "4e9c0e337136ed6cf966",
+    "url": "/nestjs_root/static/js/main.829ea0e8.chunk.js"
+  },
+  {
+    "revision": "47d101afe041249a85f1",
+    "url": "/nestjs_root/static/js/runtime~main.d065f91e.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/nestjs_root/precache-manifest.ea122aa69e34b743485fcaaef8c7d05b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nestjs_root/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
